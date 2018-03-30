bc3d3bf5264005c2eab99ee2cf65251db4fcf1af
Josef Vlach <vlach.josef@gmail.com>
2018-03-30 14:48:06 +0100

    IPC update

--- serokell/lib/Serokell/Communication/IPC.hs
+++ serokell/lib/Serokell/Communication/IPC.hs
@@ -1,52 +1,49 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE TupleSections #-}
 
 module Serokell.Communication.IPC
-    ( NodeId(..)
-    , Recv
-    , Send
-    , Conversation(..)
-    , SocketDirectory
-    , connectToUnixSocket
-    , listenUnixSocket
-    ) where
+       ( NodeId (..)
+       , Recv
+       , Send
+       , Conversation (..)
+       , SocketDirectory
 
-import Control.Concurrent (threadDelay)
-import Control.Concurrent.QSem
-       (QSem, newQSem, signalQSem, waitQSem)
-import Control.Exception.Safe (bracket, bracket_)
-import Control.Monad (when)
-import Data.Binary (Binary)
-import Data.ByteString (ByteString)
-import System.Environment (lookupEnv)
-import System.FilePath ((<.>), (</>))
-import System.IO.Unsafe (unsafePerformIO)
-import Text.Read (readMaybe)
+       , connectToUnixSocket
+       , listenUnixSocket
+       ) where
 
-import qualified Network.Socket as Net
+import           Control.Concurrent        (threadDelay)
+import           Control.Concurrent.QSem   (QSem, newQSem, signalQSem, waitQSem)
+import           Control.Exception.Safe    (bracket, bracket_)
+import           Control.Monad             (when)
+import           Data.Binary               (Binary)
+import           Data.ByteString           (ByteString)
+import           Data.Foldable             (traverse_)
+import           System.Directory          (getPermissions, executable)
+import           System.Environment        (lookupEnv, setEnv)
+import           System.FilePath           ((<.>), (</>))
+import           System.IO.Unsafe          (unsafePerformIO)
+import           Text.Read                 (readMaybe)
+
+import qualified Network.Socket            as Net
 import qualified Network.Socket.ByteString as NetByte
 
 -- | Type alias for file path, representing directory with sockets.
 type SocketDirectory = FilePath
 
 -- | Id of node.
-newtype NodeId = NodeId
-    { unNodeId :: Int
-    } deriving (Eq, Ord, Binary)
+newtype NodeId = NodeId { unNodeId :: Int }
+    deriving (Eq, Ord, Binary)
 
 type Send = ByteString -> IO ()
-
 type Recv = IO ByteString
 
 -- | Newtype wrapper for sending action to socket.
 data Conversation = Conversation
-    { send :: ByteString -> IO () -- ^ sends given 'ByteString' to socket
-    , recv :: IO ByteString -- ^ recieves 'ByteString' with length up to 4096 bytes from socket
+    { send :: ByteString -> IO ()  -- ^ sends given 'ByteString' to socket
+    , recv :: IO ByteString  -- ^ recieves 'ByteString' with length up to 4096 bytes from socket
     }
 
-readEnv
-    :: Read a
-    => String -> IO (Maybe a)
+readEnv :: Read a => String -> IO (Maybe a)
 readEnv name = (>>= readMaybe) <$> lookupEnv name
 
 -- blocking QSem for multithreaded sending environment
@@ -55,41 +52,30 @@ sendSem :: Maybe QSem
 sendSem = unsafePerformIO $ readEnv "SRK_SOCK_CONC" >>= traverse newQSem
 
 {-# NOINLINE sendSem #-}
+
 -- delay on outbound send (in milliseconds)
 -- reads env variable SRK_SOCK_DELAY
 sendDelay :: Maybe Int
 sendDelay = unsafePerformIO $ readEnv "SRK_SOCK_DELAY"
 
 {-# NOINLINE sendDelay #-}
+
 withQSem :: QSem -> IO a -> IO a
 withQSem qsem = bracket_ (waitQSem qsem) (signalQSem qsem)
 
-outboundSendRecv :: Net.Socket -> Conversation
-outboundSendRecv socket = Conversation outSend (recvSock socket)
-  where
-    outSend :: Send
-    outSend data_ =
-        (maybe id withQSem) sendSem $ do
-            _ <- traverse msDelay sendDelay
-            sendDo data_
-    sendDo = sendSock socket
-    msDelay :: Int -> IO ()
-    msDelay milliseconds = threadDelay $ milliseconds * 10 ^ (3 :: Int)
-
 sendSock :: Net.Socket -> Send
-sendSock conn = NetByte.sendAll conn
+sendSock = NetByte.sendAll
 
 recvSock :: Net.Socket -> Recv
 recvSock conn = NetByte.recv conn 4096
 
-acceptServerSock :: Net.Socket -> IO Conversation
-acceptServerSock socket = do
-    (conn, _) <- Net.accept socket
-    return $ Conversation (sendSock conn) (recvSock conn)
-
 -- Creates socket using provided allocator and handles all exceptions using bracket function.
-initializeSocket :: IO Net.Socket -> (Net.Socket -> IO ()) -> IO ()
+initializeSocket :: IO Net.Socket -> (Net.Socket -> IO a) -> IO a
 initializeSocket before = bracket before Net.close
+
+-- Local variable for storing NodeId of current node
+localSocketFilePath :: FilePath
+localSocketFilePath = "SOCKET_FILE"
 
 -- | Create new UNIX socket for node 'NodeId', listen to it and use supplied
 -- handler to process incoming connections.
@@ -100,12 +86,21 @@ listenUnixSocket :: SocketDirectory
 listenUnixSocket socketDirectory (NodeId nodeId) handler =
     initializeSocket allocator acceptLoop
   where
-    acceptLoop socket =
-        acceptServerSock socket >>= handler >>= flip when (acceptLoop socket)
+    socketPath :: FilePath
+    socketPath = socketDirectory </> show nodeId <.> "sock"
+
+    acceptLoop :: Net.Socket -> IO ()
+    acceptLoop socket = acceptServerSock >>= handler >>= flip when (acceptLoop socket)
+      where
+        acceptServerSock :: IO Conversation
+        acceptServerSock = do
+          setEnv localSocketFilePath socketPath
+          (conn, _) <- Net.accept socket
+          return $ Conversation (sendSock conn) (recvSock conn)
+
     allocator :: IO Net.Socket
     allocator = do
         socket <- Net.socket Net.AF_UNIX Net.Stream Net.defaultProtocol
-        let socketPath = socketDirectory </> show nodeId <.> "sock"
         Net.bind socket $ Net.SockAddrUnix socketPath
         Net.listen socket 5
         return socket
@@ -113,14 +108,41 @@ listenUnixSocket socketDirectory (NodeId nodeId) handler =
 -- | Connect and communicate to unix socket for node 'NodeId', handler is supplied.
 connectToUnixSocket :: SocketDirectory
                     -> NodeId
-                    -> (Conversation -> IO ())
-                    -> IO ()
+                    -> (Conversation -> IO a)
+                    -> IO a
 connectToUnixSocket socketDirectory (NodeId nodeId) handler =
     initializeSocket allocator (handler . outboundSendRecv)
   where
+    socketPath :: FilePath
+    socketPath = socketDirectory </> show nodeId <.> "sock"
+
+    outboundSendRecv :: Net.Socket -> Conversation
+    outboundSendRecv socket = Conversation outSend (recvSock socket)
+      where
+        outSend :: Send
+        outSend data_ =
+            maybe id withQSem sendSem $ do
+                mLocalSocketFile <- lookupEnv localSocketFilePath
+
+                executableLocalFile <- case mLocalSocketFile of
+                  Nothing -> pure True
+                  Just localSocketFile -> executable <$> getPermissions localSocketFile
+
+                permission <- getPermissions socketPath
+                if executable permission && executableLocalFile
+                  then do
+                    traverse_ msDelay sendDelay
+                    sendDo data_
+                  else Net.close socket
+
+        sendDo :: ByteString -> IO ()
+        sendDo = sendSock socket
+
+        msDelay :: Int -> IO ()
+        msDelay milliseconds = threadDelay $ milliseconds * 10^(3 :: Int)
+
     allocator :: IO Net.Socket
     allocator = do
         socket <- Net.socket Net.AF_UNIX Net.Stream Net.defaultProtocol
-        let socketPath = socketDirectory </> show nodeId <.> "sock"
         Net.connect socket $ Net.SockAddrUnix socketPath
         return socket
